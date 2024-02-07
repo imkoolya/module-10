b6e73ef749e0f7a9a5151180ca3742e33e0facbf
imkoolya <imkoolya@gmail.com>
2024-02-07 16:10:30 +0300

    Add project files.

---
+++ module-10/Program.cs
@@ -0,0 +1,39 @@
+﻿interface ICalculator
+{
+    int Add(int a, int b);
+}
+class BasicCalculator : ICalculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Введите первое число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            ICalculator calculator = new BasicCalculator();
+            int result = calculator.Add(a, b);
+
+            Console.WriteLine("Результат сложения: {0}", result);
+        }
+        catch
+        {
+            Console.WriteLine("Ошибка: Неверный формат.");
+        }
+        finally
+        {
+            Console.WriteLine("Работа калькулятора завершена.");
+        }
+    }
+}
